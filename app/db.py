7fe934c9a568d0eeca1e5bb699efc7d0b890a9e8
Zahtec <32808999+zahtec@users.noreply.github.com>
2022-07-12 13:04:34 -0700

    updates to main.py

---
+++ app/db.py
@@ -0,0 +1,15 @@
+# this is where we put all of the info about the snakes and mushrooms
+
+db = {
+    'Crotalus atrox': {
+        'species': 'Crotalus atrox',
+        'type': 'snake',
+        'poisionous': True,
+        'cookable': True,
+        'description': 'Crotalus atrox, common known as the Western diamondback rattlesnakes, are non-aggressive venomous rattlesnakes. They are found in south-western parts of the US as well as Mexico. The Crotalus atrox are responsible for the greatest number of snakebites in the US.',
+        'steps': 'Best course of action is to not interfere with snake. Leave its surroundings immediately.Most rattlesnakes, when disturbed, normally try to withdraw, but if they think they are cornered, the explosive sizzling buzz of their rattle is an unmistakable warning to retreat and is a sound that will long be remembered.',
+        'bitten': "The venom from the majority of rattlesnake bites will damage tissue and affect your circulatory system by destroying skin tissues and blood cells and by causing you to hemorrhage internally.  The first and most important thing to do is get away from the snake, as they can strike again if they feel threatened. SEEK MEDICAL ASSISTANCE IMMEDIATELY!",
+        'image': 'https://live.staticflickr.com/3870/14688594389_3cd92a0323_b.jpg',
+        'article': 'https://en.wikipedia.org/wiki/Masticophis_flagellum'
+    },
+}
--- app/main.py
+++ app/main.py
@@ -1,113 +1,88 @@
-from flask import send_from_directory
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, request, send_from_directory, make_response
 from werkzeug.utils import secure_filename
-from flask import render_template
-from url_utils import get_base_url
-import os
+from flask import send_from_directory
+from json import dumps
+from db import db
 import torch
+import os
 
 # setup the webserver
 # port may need to be changed if there are multiple flask servers running on same server
 port = 12345
-base_url = get_base_url(port)
 
 # if the base url is not empty, then the server is running in development, and we need to specify the static folder so that the static files are served
-if base_url == '/':
-    app = Flask(__name__)
-else:
-    app = Flask(__name__, static_url_path=base_url+'static')
+app = Flask(__name__)
 
-UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024
 
-model = torch.hub.load("ultralytics/yolov5", "custom", path = 'best.pt', force_reload=True)
+model = torch.hub.load("ultralytics/yolov5", "custom",
+                       path='best.pt', force_reload=True)
+
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route(f'{base_url}', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-
-    return render_template('home.html')
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ['png', 'jpg', 'jpeg']
 
 
-@app.route(f'{base_url}/uploads/<filename>')
-def uploaded_file(filename):
-    here = os.getcwd()
-    image_path = os.path.join(here, app.config['UPLOAD_FOLDER'], filename)
-    results = model(image_path, size=416)
-    if len(results.pandas().xyxy) > 0:
-        results.print()
-        save_dir = os.path.join(here, app.config['UPLOAD_FOLDER'])
-        results.save(save_dir=save_dir)
-        def and_syntax(alist):
-            if len(alist) == 1:
-                alist = "".join(alist)
-                return alist
-            elif len(alist) == 2:
-                alist = " and ".join(alist)
-                return alist
-            elif len(alist) > 2:
-                alist[-1] = "and " + alist[-1]
-                alist = ", ".join(alist)
-                return alist
-            else:
-                return
-        confidences = list(results.pandas().xyxy[0]['confidence'])
-        # confidences: rounding and changing to percent, putting in function
-        format_confidences = []
-        for percent in confidences:
-            format_confidences.append(str(round(percent*100)) + '%')
-        format_confidences = and_syntax(format_confidences)
+@app.route('/', methods=['POST'])
+def post():
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    if file and allowed_file(file.filename):
+        path = os.path.join('./static/uploads', filename)
+        file.save(path)
+        results = model(path, size=416)
+        if len(results.pandas().xyxy) > 0:
+            results.print()
+            results.save(save_dir='./static/images')
 
-        labels = list(results.pandas().xyxy[0]['name'])
-        # labels: sorting and capitalizing, putting into function
-        labels = set(labels)
-        labels = [emotion.capitalize() for emotion in labels]
-        labels = and_syntax(labels)
-        return render_template('results.html', confidences=format_confidences, labels=labels,
-                               old_filename=filename,
-                               filename=filename)
+            def and_syntax(alist):
+                if len(alist) == 1:
+                    alist = "".join(alist)
+                    return alist
+                elif len(alist) == 2:
+                    alist = " and ".join(alist)
+                    return alist
+                elif len(alist) > 2:
+                    alist[-1] = "and " + alist[-1]
+                    alist = ", ".join(alist)
+                    return alist
+                else:
+                    return
+
+            confidences = list(results.pandas().xyxy[0]['confidence'])
+            # confidences: rounding and changing to percent, putting in function
+            format_confidences = []
+            for percent in confidences:
+                format_confidences.append(str(round(percent*100)) + '%')
+            format_confidences = and_syntax(format_confidences)
+
+            labels = list(results.pandas().xyxy[0]['name'])
+            # labels: sorting and capitalizing, putting into function
+            labels = set(labels)
+            labels = and_syntax(labels)
+            print(labels)
+            res = make_response(dumps(db[labels].update(
+                {'upload': f'images/{filename}', 'confidence': format_confidences})), 200)
+            res.headers['Content-Type'] = 'application/json'
+            return res
+        else:
+            return make_response('NotFound', 200)
     else:
-        found = False
-        return render_template('results.html', labels='No Emotion', old_filename=filename, filename=filename)
+        return make_response('Invalid file type or missing file', 400)
 
 
-@app.route(f'{base_url}/uploads/<path:filename>')
-def files(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
+@app.route('/images/<path>')
+def images(path):
+    return send_from_directory('./static/images', path)
 
-# define additional routes here
-# for example:
-# @app.route(f'{base_url}/team_members')
-# def team_members():
-#     return render_template('team_members.html') # would need to actually make this page
+
+@app.route('/', methods=['GET'])
+def home():
+    return send_from_directory('./static', 'index.html')
+
 
 if __name__ == '__main__':
     # IMPORTANT: change url to the site where you are editing this file.
-    website_url = 'url'
-    
-    print(f'Try to open\n\n    https://{website_url}' + base_url + '\n\n')
-    app.run(host = '0.0.0.0', port=port, debug=True)
+    website_url = 'cocalc14.ai-camp.dev'
+    print(f'Try to open\n\n    https://{website_url}' + '\n\n')
+    app.run(host='localhost', port=port, debug=True)
